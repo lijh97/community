540af199101d41ff199c9819b17c459d42f47528
lijh97 <1172904580@qq.com>
2019-09-03 00:20:30 +0800

    use lombok plugin to rebuild project

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,15 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.UserMapper;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/index")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if("token".equals(cookie.getName())){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
         return "index";
     }
 }
--- demo/src/main/java/com/example/demo/dto/GithubUser.java
+++ demo/src/main/java/com/example/demo/dto/GithubUser.java
@@ -1,31 +1,11 @@
 package com.example.demo.dto;
 
+import lombok.*;
+
+@Data
 public class GithubUser {
     private String name;
     private String bio;
     private long id;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
+    private String avatarUrl;
 }
